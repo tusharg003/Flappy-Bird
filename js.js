cf3f90f305e18da68d4e3fc613b835d7b45cda14
Tushar Gupta <tushargupta4870@gmail.com>
2023-09-07 01:35:58 +0530

    added the pipes and their animation

--- js.js
+++ js.js
@@ -16,6 +16,19 @@ let bird = {
     height: birdHeight
 }
 
+//pipes
+let pipeArr = [];
+let pipeWidth = 64;
+let pipeHeight = 512;
+let pipeX = boardWidth;
+let pipeY = 0;
+
+let topPipeImg;
+let bottomPipeImg;
+
+//physics
+let velocityX = -2;
+
 window.onload = () => {
     board = document.querySelector('#board');
     board.height = boardHeight;
@@ -33,4 +46,53 @@ window.onload = () => {
         context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
     }
 
+    topPipeImg = new Image();//image object's instance
+    topPipeImg.src = "./assets/toppipe.png";
+
+    bottomPipeImg = new Image();
+    bottomPipeImg.src = "./assets/bottompipe.png";
+
+    requestAnimationFrame(update);
+    setInterval(placePipes, 1000);
+}
+
+function update() {
+    requestAnimationFrame(update);
+    context.clearRect(0, 0, boardWidth, boardHeight);
+
+    //bird
+    context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
+
+    //pipes
+    for (let i = 0; i < pipeArr.length; i++) {
+        let pipe = pipeArr[i];
+        pipe.x += velocityX;
+        context.drawImage(pipe.img, pipe.x, pipe.y, pipe.width, pipe.height);
+    }
+}
+
+function placePipes() {
+
+    let randomPipeY = pipeY - pipeHeight / 4 - Math.random() * (pipeHeight / 2);
+    let openingSpace = board.height / 4;
+
+    let topPipe = {
+        img: topPipeImg,
+        x: pipeX,
+        y: randomPipeY,
+        width: pipeWidth,
+        height: pipeHeight,
+        passed: false
+    }
+    pipeArr.push(topPipe);
+
+    let bottomPipe = {
+        img: bottomPipeImg,
+        x: pipeX,
+        y: randomPipeY + pipeHeight + openingSpace,
+        width: pipeWidth,
+        height: pipeHeight,
+        passed: false
+    }
+    pipeArr.push(bottomPipe);
 }
